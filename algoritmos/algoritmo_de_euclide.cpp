86d3c8aa48e623c9548621f3e89e547de3f76383
Francisco Jair <fjair123@ufpi.edu.br>
2020-08-31 20:36:09 -0300

    Algoritmo de Euclides

---
+++ algoritmos/algoritmo_de_euclide.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+int mdc(int x, int y)
+{
+    if (y == 0)
+    {
+        return x;
+    }
+    else
+    {
+        return mdc(y, x % y);
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    cout << mdc(1000, 550);
+
+    return 0;
+}
